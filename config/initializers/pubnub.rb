522388059f2752703c041afb4930a89d931ecf23
Bhuvnesh Varshney <developerbhuv@gmail.com>
2018-06-25 03:35:07 +0530

    fixing syntax issue with pubnub.rb

--- config/initializers/pubnub.rb
+++ config/initializers/pubnub.rb
@@ -1,5 +1,5 @@
 require 'pubnub'
 $pubnub = Pubnub.new(
-   subscribe_key: : Figaro.env.subscribe_key,
-   publish_key: : Figaro.env.publish_key
+   subscribe_key: Figaro.env.subscribe_key,
+   publish_key: Figaro.env.publish_key
 )
